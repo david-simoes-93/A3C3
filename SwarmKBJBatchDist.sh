fc664f857c37944c480ed56c0a0c25d9e50d3856
bluemoon93 <david.simoes@ua.pt>
2019-03-18 15:19:29 +0000

    Laucnh

---
+++ SwarmKBJBatchDist.sh
@@ -0,0 +1,21 @@
+export PYTHONPATH=$(pwd)
+
+thread_number=12
+count=`expr $thread_number - 1`
+
+for trial in 0
+do
+	for comm in 2
+	do
+		for i in `seq 0 $count`
+		do
+		    python3 KiloBotsJoinSwarm/MA3C-Distributed.py --comm_size="$comm" --slaves_per_url="$thread_number" --urls=localhost --task_index="$i" &
+		    echo python3 KiloBotsJoinSwarm/MA3C-Distributed.py --task_max="$thread_number" --task_index="$i"
+		    sleep 5
+		done
+		wait
+	    sleep 5
+	    mkdir KiloBotsJoinSwarm-"$comm"Comm-Trial"$trial"
+	    mv dist_model train_* KiloBotsJoinSwarm-"$comm"Comm-Trial"$trial"/
+	done
+done
